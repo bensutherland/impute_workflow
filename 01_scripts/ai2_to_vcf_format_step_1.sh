653255a0cd2e6553dc2e373be318c731841345f6
GreenT <bensutherland7@gmail.com>
2024-09-19 10:53:21 -0700

    Add lines to remove temp files

--- 01_scripts/ai2_to_vcf_format_step_1.sh
+++ 01_scripts/ai2_to_vcf_format_step_1.sh
@@ -2,8 +2,8 @@
 # Convert ai2 file to VCF file (step 1) 
 
 # Set user variables
-INPUT_FOLDER="05_compare"
-INPUT_AI2="all_chr_combined.txt"
+INPUT_FOLDER="04_impute/fimpute"
+INPUT_AI2="fi3_loci_by_inds_all_imputed_chr.txt"
 
 # Retain marker names, no header
 awk -F"\t" '{ print $1 }' $INPUT_FOLDER/$INPUT_AI2 | grep -vE '^mname' - > $INPUT_FOLDER/${INPUT_AI2%.txt}_mnames.txt
@@ -24,7 +24,9 @@ paste -d "\t" $INPUT_FOLDER/${INPUT_AI2%.txt}_mnames.txt $INPUT_FOLDER/${INPUT_A
 cat $INPUT_FOLDER/${INPUT_AI2%.txt}_header.txt $INPUT_FOLDER/${INPUT_AI2%.txt}_converted_body.txt > $INPUT_FOLDER/${INPUT_AI2%.txt}_converted.txt
 
 # Clean up
-
-
-
+rm $INPUT_FOLDER/${INPUT_AI2%.txt}_mnames.txt
+rm $INPUT_FOLDER/${INPUT_AI2%.txt}_header.txt
+rm $INPUT_FOLDER/${INPUT_AI2%.txt}_geno_only.txt
+rm $INPUT_FOLDER/${INPUT_AI2%.txt}_geno_only_converted.txt
+rm $INPUT_FOLDER/${INPUT_AI2%.txt}_converted_body.txt
 
