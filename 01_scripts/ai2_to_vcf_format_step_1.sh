00c9a46a0f77d8ce90d80adfbad4dcd98fa11dee
GreenT <bensutherland7@gmail.com>
2024-09-20 13:54:52 -0700

    Improve code structure

--- 01_scripts/ai2_to_vcf_format_step_1.sh
+++ 01_scripts/ai2_to_vcf_format_step_1.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Convert ai2 file to VCF file (step 1) 
+# note: the INPUT_AI2 variable is the post-imputation file after it has been rejoined
 
 # Set user variables
-INPUT_FOLDER="04_impute/fimpute"
-INPUT_AI2="fi3_loci_by_inds_all_imputed_chr.txt"
+INPUT_FOLDER="05_compare"
+INPUT_AI2="all_chr_combined.txt"
 
 # Retain marker names, no header
-awk -F"\t" '{ print $1 }' $INPUT_FOLDER/$INPUT_AI2 | grep -vE '^mname' - > $INPUT_FOLDER/${INPUT_AI2%.txt}_mnames.txt
+awk -F"\t" '{ print $1 }' $INPUT_FOLDER/$INPUT_AI2 | 
+        grep -vE '^mname' - > $INPUT_FOLDER/${INPUT_AI2%.txt}_mnames.txt
 
 # Retain colnames 
 head -n 1 $INPUT_FOLDER/$INPUT_AI2 > $INPUT_FOLDER/${INPUT_AI2%.txt}_header.txt
@@ -15,12 +17,14 @@ head -n 1 $INPUT_FOLDER/$INPUT_AI2 > $INPUT_FOLDER/${INPUT_AI2%.txt}_header.txt
 cut -f2- $INPUT_FOLDER/$INPUT_AI2 | tail -n +2 > $INPUT_FOLDER/${INPUT_AI2%.txt}_geno_only.txt
 
 # Convert values back to VCF-style genotypes
-sed 's/0/0\/0/g' $INPUT_FOLDER/${INPUT_AI2%.txt}_geno_only.txt | sed 's/1/0\/1/g' - | sed 's/2/1\/1/g' - > $INPUT_FOLDER/${INPUT_AI2%.txt}_geno_only_converted.txt
+sed 's/0/0\/0/g' $INPUT_FOLDER/${INPUT_AI2%.txt}_geno_only.txt | 
+        sed 's/1/0\/1/g' - | 
+        sed 's/2/1\/1/g' - > $INPUT_FOLDER/${INPUT_AI2%.txt}_geno_only_converted.txt
 
-# Recombine step 1
+# Combine data back together (step 1)
 paste -d "\t" $INPUT_FOLDER/${INPUT_AI2%.txt}_mnames.txt $INPUT_FOLDER/${INPUT_AI2%.txt}_geno_only_converted.txt > $INPUT_FOLDER/${INPUT_AI2%.txt}_converted_body.txt 
 
-# Recombine step 2
+# Combine data back together (step 2)
 cat $INPUT_FOLDER/${INPUT_AI2%.txt}_header.txt $INPUT_FOLDER/${INPUT_AI2%.txt}_converted_body.txt > $INPUT_FOLDER/${INPUT_AI2%.txt}_converted.txt
 
 # Clean up
