d7d7b30dabbc096b29aee4027598521229c81a9c
GreenT <bensutherland7@gmail.com>
2024-10-21 13:25:29 -0700

    Improve commenting in script

--- 01_scripts/imputed_plot_gemma_results.R
+++ 01_scripts/imputed_plot_gemma_results.R
@@ -27,21 +27,27 @@ setwd(current.path)
 rm(current.path)
 
 # User set variables
-gemma_output.FN       <- "07_GWAS/output/gwas_all_fam_covariate.assoc.txt"
+gemma_output.FN       <- "07_GWAS/output/gwas_all_fam.assoc.txt"
 phenotype_of_interest <- "survival_state" # DPE or survival_state
 highlight_snps.FN <- "07_GWAS/denovo_snp_ids.txt"
 caution_zones.FN <- "chr_locs_w_multimapper_loci.txt"  # This is if you are using multimappers to plot
 
 plotting_multimappers <- FALSE # Set as TRUE if you want to plot multimappers and have the files available
 
-# Read in data
+plot_maxP <- 9999999 # set the maxP cutoff for the Fastman plot
+
+
+#### 01. Read in data ####
+# Read in GWAS output
 gemma_gwas <- fread(file = gemma_output.FN, header = T)
 gemma_gwas <- as.data.frame(gemma_gwas)
 head(gemma_gwas)
 
+# Read in the SNPs to highlight (single column with marker name, e.g., 'chr__position')
 highlight_snps.df <- read.delim(file = highlight_snps.FN, header = F, sep = "\t")
 head(highlight_snps.df)
 
+# Read in multimapper caution zones if using
 if(plotting_multimappers == TRUE){
   
   caution_zones.df <- read.delim(file = caution_zones.FN, header = T, sep = "\t")
@@ -51,13 +57,14 @@ if(plotting_multimappers == TRUE){
 }
 
 
-# Convert mname into chr and pos
+#### 02. Format data as needed ####
+# Convert gemma output file mname into chr and pos
 gemma_gwas <- separate(data = gemma_gwas, col = "rs", into = c("chr", "pos"), sep = "__", remove = F)
 head(gemma_gwas)
 gemma_gwas$pos <- as.numeric(gemma_gwas$pos)
 
-# convert to linkage group (LG), based on https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_902806645.1/
-# LG to Chr info can be found in Sup File in https://doi.org/10.1093/gigascience/giab020
+# Convert to linkage group (LG), based on https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_902806645.1/
+# LG to chr info can be found in Sup File in https://doi.org/10.1093/gigascience/giab020
 gemma_gwas$chr <- gsub(pattern = "NC_047559.1", replacement = "Chr07", x = gemma_gwas$chr)
 gemma_gwas$chr <- gsub(pattern = "NC_047560.1", replacement = "Chr01", x = gemma_gwas$chr)
 gemma_gwas$chr <- gsub(pattern = "NC_047561.1", replacement = "Chr09", x = gemma_gwas$chr)
@@ -71,34 +78,32 @@ gemma_gwas$chr <- gsub(pattern = "NC_047568.1", replacement = "Chr08", x = gemma
 
 gemma_gwas <- gemma_gwas[with(gemma_gwas, order(gemma_gwas$chr)), ]
 
-hist(gemma_gwas$p_wald, breaks = 20)
 
+#### 03. Plotting ####
+# Plot p-val histogram
+pdf(file = paste0("07_GWAS/pval_hist_", phenotype_of_interest, ".pdf"), width = 7.5, height = 4.5)
+hist(gemma_gwas$p_wald, breaks = 20)
+dev.off()
+
+# Plot Manhattan plot (default)
 pdf(file = paste0("07_GWAS/Manhattan_plot_", phenotype_of_interest, ".pdf"), width = 11, height = 7)
 par(mfrow = c(1,1), mar = c(5,4,4,2) +0.1, mgp = c(3,1,0))
 fastman(m = gemma_gwas, chr = "chr", bp = "pos", p = "p_wald"
         , genomewideline = -log10(0.05/nrow(gemma_gwas))
         #, suggestiveline = -log10(0.05)
         , cex = 0.7, cex.lab = 1, cex.axis = 1
-        , ylim = c(0,10)
+        #, ylim = c(0,10)
+        , maxP = plot_maxP
         )
 dev.off()
 
 
-
-head(gemma_gwas)
-# colnames(gemma_gwas)[which(colnames(gemma_gwas)=="rs")] <- "SNP"
-head(gemma_gwas)
-
-#highlight_vec <- gemma_gwas[which(gemma_gwas$p_wald==min(gemma_gwas$p_wald)), "SNP"]
+# Create highlight SNP vector, making sure that they are actually in the gemma output, and accounting
 highlight.vec <- highlight_snps.df$V1
 length(highlight.vec)
 highlight.vec <- highlight.vec[highlight.vec %in% gemma_gwas$rs]
 length(highlight.vec)
 
-# gemma_gwas$cex <- gemma_gwas$SNP %in% highlight.vec
-# gemma_gwas$cex[which(gemma_gwas$cex==TRUE)] <- 1.5
-# gemma_gwas$cex[which(gemma_gwas$cex==0)] <- 0.7
-# table(gemma_gwas$cex)
 
 pdf(file = paste0("07_GWAS/Manhattan_plot_", phenotype_of_interest, "_annotated.pdf"), width = 10.8, height = 5.2)
 par(mfrow = c(1,1), mar = c(5,4,4,2) +0.1, mgp = c(3,1,0))
@@ -118,17 +123,21 @@ fastman(m = gemma_gwas, chr = "chr", bp = "pos", p = "p_wald", snp = "rs"
         #, annotateTop=FALSE
         , annotationCol = "black"
         , annotationAngle= 55
+        , maxP = plot_maxP
 )
 dev.off()
 
+
+
+#### 04. Result retention ####
 # Write out table with whether SNP is from panel
 gemma_gwas$panel_snp <- gemma_gwas$rs %in% highlight.vec
 write.table(x = gemma_gwas, file = gsub(pattern = "\\.txt", replacement = "_w_annot.txt", x =  gemma_output.FN)
             , sep = "\t", quote = F, row.names = F, col.names = T
             )
 
-#### Multimapper caution zones ####
 
+#### 05. Plotting multimapper caution zones ####
 if(plotting_multimappers == TRUE){
   
   
@@ -195,13 +204,14 @@ if(plotting_multimappers == TRUE){
                 #, annotateTop=FALSE
                 , annotationCol = "black"
                 , annotationAngle= 55
+                , maxP = plot_maxP
         )
         dev.off()
 }
 
 
 
-
+#### 06. Save out final results ####
 # Write out as R object so can be easily loaded back
 save.image(file = gsub(pattern = "\\.txt", replacement = ".RData", x = gemma_output.FN))
 # Downstream will use gemma_gwas object to multi-pannel plot GWAS output
