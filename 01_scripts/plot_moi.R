39c97ee8a903c9803abaed1870fca62ab8fb9687
GreenT <bensutherland7@gmail.com>
2024-10-25 11:29:59 -0700

    Enable methods of plotting different input datatypes (empirical or imputed) or targets

--- 01_scripts/plot_moi.R
+++ 01_scripts/plot_moi.R
@@ -20,9 +20,21 @@ setwd(current.path)
 rm(current.path)
 
 # Set user variables
-VCF.FN    <- "plot_mois/all_inds_wgrs_and_panel_biallele_only_fi3_imputed_NC_047568.1.vcf.gz"
 interp.FN <- "00_archive/G0923-21-VIUN_SampleInventory_V2_recd_2024-08-16.txt"
 
+# Options
+plot_targets <- "high_sig" # chr8_locus or high_sig
+datatype <- "empirical" # empirical or imputed
+
+if(datatype=="empirical"){
+  
+  VCF.FN    <- "plot_mois/mpileup_empirical_input_offspring_NC_047568.1.vcf.gz" # empirical
+  
+}else if(datatype=="imputed"){
+  
+  VCF.FN    <- "plot_mois/all_inds_wgrs_and_panel_biallele_only_fi3_imputed_NC_047568.1.vcf.gz" # imputed
+  
+}
 
 #### 01. Load data ####
 # VCF file
@@ -41,10 +53,27 @@ head(interp.df)
 interp.df <- interp.df[!is.na(interp.df$survival_state), ]
 
 # Identify which outliers to plot
-moi <- c("NC_047568.1__42154072", "NC_047568.1__42154146", "NC_047568.1__42154212", "NC_047568.1__42154223" # top loci
-         , "NC_047568.1__12786623", "NC_047568.1__15840611", "NC_047568.1__39651092", "NC_047568.1__35480830" # top panel loci (1)
-         , "NC_047568.1__5525383",  "NC_047568.1__28802499" # top panel loci (2)
-)
+
+if(plot_targets == "high_sig"){
+  
+  # High significance outliers
+  moi <- c("NC_047568.1__42154072", "NC_047568.1__42154146", "NC_047568.1__42154212", "NC_047568.1__42154223" # top loci
+           , "NC_047568.1__12786623", "NC_047568.1__15840611", "NC_047568.1__39651092", "NC_047568.1__35480830" # top panel loci (1)
+           , "NC_047568.1__5525383",  "NC_047568.1__28802499" # top panel loci (2)
+           , "NC_047568.1__19955209" # extreme outlier when no family covariate
+  )
+  
+  
+}else if(plot_targets == "chr8_locus"){ 
+  
+  # Loci near chr8 locus
+  moi <- c("NC_047568.1__9718924", "NC_047568.1__9718930", "NC_047568.1__9719319"
+           , "NC_047568.1__9719440", "NC_047568.1__9719458", "NC_047568.1__9719799"
+           , "NC_047568.1__9720048")
+  
+  }
+
+
 
 
 #### 02. Extract genotypes from VCF file
@@ -58,8 +87,9 @@ my_vcf@fix <- my_vcf.df
 head(my_vcf@fix)
 
 # Extract genotypes
-genos.df <- extract.gt(x = my_vcf)
-head(genos.df)
+genos.df <- extract.gt(x = my_vcf, element = "GT")
+dim(genos.df)
+genos.df[1:5,1:5]
 
 # Extract only the mois
 mois.df <- genos.df[(rownames(genos.df) %in% moi), ]
@@ -77,6 +107,7 @@ mois.df <- gsub(pattern = "1/1", replacement = "2", x = mois.df)
 mois.df[1:5,1:5]
 
 mode(mois.df) <- "numeric"
+mois.df[1:5,1:5]
 str(mois.df)
 
 # Convert to df
@@ -90,56 +121,51 @@ mois.df[1:5,1:5]
 head(interp.df)
 
 plot_data.df <- merge(x = interp.df, y = mois.df, by = "indiv")
-head(plot_data.df)
+dim(plot_data.df)
+plot_data.df[1:5,1:10]
 
-# TODO make a survival state x fam variable
+
 
 ##### PLOT #####
-# p <- ggplot(plot_data.df, aes(x = survival_state, y = as.factor(NC_047568.1__5525383))) + 
-#   geom_jitter(height = 0, width = 0.25, alpha = 0.5, size = 3) + 
-#   theme_classic()
-# p
-# 
-# p <- ggplot(plot_data.df, aes(x = survival_state, y = NC_047568.1__5525383)) + 
-#   geom_boxplot(fill = "grey") +
-#   theme_classic()
-# p
-
+# These are the available markers of interest
 moi
 
-# To show that its working 
-p <- ggplot(plot_data.df, aes(x = fam.x.surv, y = NC_047568.1__42154072)) + 
-  geom_boxplot(fill = "grey") + 
-  geom_jitter(height = 0.1, width = 0.25, alpha = 0.5, size = 3) + 
-  theme_classic() +
-  ylim(-0.5,2.5)
-p
+# In the case of empirical, some of these may not actually be present in the data
+present_moi <- colnames(plot_data.df)[grep(pattern = "NC_047", x = colnames(plot_data.df))]
+moi <- present_moi
+# # To show that its working 
+# p <- ggplot(plot_data.df, aes(x = fam.x.surv, y = NC_047568.1__19955209)) + 
+#   geom_boxplot(fill = "grey") + 
+#   geom_jitter(height = 0.1, width = 0.25, alpha = 0.5, size = 3) + 
+#   theme_classic() +
+#   ylim(-0.5,2.5)
+# p
 
 
-plot.list <- list()
-marker <- NULL; p <- NULL
-
-for(i in 1:length(moi)){
-  
-  print(paste0("Plotting genotypes for marker ", moi[i]))
-  
-  marker <- moi[i]
-  marker <- sym(marker)
-  
-  p <- ggplot(plot_data.df, aes(x = fam.x.surv, y = !!marker)) + 
-    geom_boxplot(fill = "grey") + 
-    geom_jitter(height = 0.1, width = 0.25, alpha = 0.5, size = 3) + 
-    theme_classic() +
-    ylim(-0.5,2.5)
-  
-  print(p)
-  
-  plot.list[[moi[i]]] <- p
-  
-}
-
-names(plot.list)
-print(plot.list[10])
+# plot.list <- list()
+# marker <- NULL; p <- NULL
+# 
+# for(i in 1:length(moi)){
+#   
+#   print(paste0("Plotting genotypes for marker ", moi[i]))
+#   
+#   marker <- moi[i]
+#   marker <- sym(marker)
+#   
+#   p <- ggplot(plot_data.df, aes(x = fam.x.surv, y = !!marker)) + 
+#     geom_boxplot(fill = "grey") + 
+#     geom_jitter(height = 0.1, width = 0.25, alpha = 0.5, size = 3) + 
+#     theme_classic() +
+#     ylim(-0.5,2.5)
+#   
+#   print(p)
+#   
+#   plot.list[[moi[i]]] <- p
+#   
+# }
+# 
+# names(plot.list)
+# print(plot.list[1])
 
 
 #### Violin plot option ####
@@ -157,7 +183,9 @@ for(i in 1:length(moi)){
     geom_violin(fill = "grey") + 
     geom_jitter(height = 0.1, width = 0.25, alpha = 0.5, size = 3) + 
     theme_classic() +
-    ylim(-0.5,2.5)
+    ylim(-0.5,2.5) + 
+    theme(axis.text.x = element_text(angle = -90, hjust = 0)) +
+    ggtitle(NULL)
   
   print(p)
   
@@ -165,37 +193,36 @@ for(i in 1:length(moi)){
   
 }
 
-names(plot.list)
-print(plot.list[10])
+names(violin_plot.list)
+print(violin_plot.list[1])
 
+plot_num <- seq(1:length(violin_plot.list))
 
-final_figure <- ggarrange(violin_plot.list[[1]], violin_plot.list[[2]], violin_plot.list[[3]]
-          , violin_plot.list[[4]], violin_plot.list[[5]], violin_plot.list[[6]]
-          , violin_plot.list[[7]], violin_plot.list[[8]], violin_plot.list[[9]]
-          , ncol = 3, nrow = 3, labels = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"))
+if(plot_targets=="high_sig"){
+  
+  final_figure <- ggarrange(violin_plot.list[[1]], violin_plot.list[[2]], violin_plot.list[[3]]
+                            , violin_plot.list[[4]], violin_plot.list[[5]], violin_plot.list[[6]]
+                            , violin_plot.list[[7]], violin_plot.list[[8]]
+                            #, violin_plot.list[[9]]
+                            #, violin_plot.list[[10]], violin_plot.list[[11]]
+                            , ncol = 4, nrow = 3, labels = c("A", "B", "C", "D", "E", "F", "G", "H"
+                                                             #, "I", "J", "K", "L", "M", "N"
+                                                             ))
+  
+  pdf(file = paste0("plot_mois/multiple_loci_pheno_by_geno_violin_", plot_targets, "_", datatype, ".pdf"), width = 12, height = 9)
+  print(final_figure)
+  dev.off()
+  
+}else if(plot_targets=="chr8_locus"){
+  
+  final_figure <- ggarrange(violin_plot.list[[1]], violin_plot.list[[2]], violin_plot.list[[3]]
+                              , violin_plot.list[[4]], violin_plot.list[[5]], violin_plot.list[[6]]
+                              , violin_plot.list[[7]]
+                              , ncol = 3, nrow = 3, labels = c("A", "B", "C", "D", "E", "F", "G"))
+    
+  pdf(file = paste0("plot_mois/multiple_loci_pheno_by_geno_violin_", plot_targets, "_", datatype, ".pdf"), width = 7, height = 7)
+  print(final_figure)
+  dev.off()
+    
+}
 
-pdf(file = "multiple_loci_pheno_by_geno_violin.pdf", width = 12, height = 12)
-print(final_figure)
-dev.off()
-
-
-
-
-
-
-
-
-# 
-# marker <- "NC_047568.1__42154072"
-# marker <- sym(marker)
-# 
-# 
-# "NC_047568.1__5525383"
-# p <- ggplot(plot_data.df, aes(x = fam.x.surv, y = NC_047568.1__5525383)) + 
-#   geom_boxplot(fill = "grey") + 
-#   geom_jitter(height = 0.1, width = 0.25, alpha = 0.5, size = 3) + 
-#   theme_classic() +  ylim(-0.5,2.5)
-# p
-# 
-# "NC_047568.1__42154146"
-# 
