73c433b38dca570dd71a18d9db51bd2e0fb3878c
GreenT <bensutherland7@gmail.com>
2024-12-13 13:13:27 -0800

    Put post-impute sample order back into pre-impute sample order before inserting GT matrix into VCF obj

--- 01_scripts/ai2_to_VCF.R
+++ 01_scripts/ai2_to_VCF.R
@@ -20,19 +20,17 @@ setwd(current.path)
 rm(current.path)
 
 # Set user variables
-#pre_impute_VCF.FN  <- "04_impute/all_inds_wgrs_and_panel_biallele_only_no_MERR_ai2_imputed_regions.vcf" # this is the pre-impute VCF, subset to only incl the loci retained after the ai2 imputation
-#pre_impute_VCF.FN  <- "04_impute/all_inds_wgrs_and_panel_biallele_only_fi3_imputed_regions.vcf" # this is the pre-impute VCF, subset to only incl the loci retained after the ai2 imputation
-pre_impute_VCF.FN  <- "04_impute/all_inds_wgrs_and_panel_biallele_only_no_MERR_fi3_imputed_regions.vcf" # this is the pre-impute VCF, subset to only incl the loci retained after the ai2 imputation
-#post_impute_ai2.FN <- "05_compare/all_chr_combined_converted.txt"
+## AI2 settings 
+# pre_impute_VCF.FN  <- "04_impute/all_inds_wgrs_and_panel_biallele_only_ai2_imputed_regions.vcf" # this is the pre-impute VCF, subset to only incl the loci retained after the ai2 imputation
+# post_impute_ai2.FN <- "05_compare/all_chr_combined_converted.txt"
+
+## FI3 settings
+pre_impute_VCF.FN  <- "04_impute/all_inds_wgrs_and_panel_biallele_only_fi3_imputed_regions.vcf" # this is the pre-impute VCF, subset to only incl the loci retained after the fi3 imputation
 post_impute_ai2.FN <- "04_impute/fimpute/fi3_loci_by_inds_all_imputed_chr_converted.txt"
 
 
 #### 01. Load data ####
-# Load VCF file
-pre_vcf <- read.vcfR(file = pre_impute_VCF.FN)
-#pre_vcf.bck <- pre_vcf
-gc()
-
+## AI2-format imputed data ##
 # Load AI2 output file
 ai2_output.df <- fread(file = post_impute_ai2.FN, sep = "\t")
 dim(ai2_output.df)
@@ -40,15 +38,22 @@ ai2_output.df$mname <- gsub(pattern = " ", replacement = "__", x = ai2_output.df
 ai2_output.df[1:5,1:5]
 gc()
 
+# Load VCF file
+pre_vcf <- read.vcfR(file = pre_impute_VCF.FN)
+gc()
+
 # View GT section
 dim(pre_vcf@gt)
 pre_vcf@gt[1:5,1:5]
+
+# NOTE: the AI2 and the pre-imputed VCF may NOT have the same sample order, as it may have been 
+#  sorted by alphaimpute2 (2024-12-13, bjgs)
 
 # View Info section
 dim(pre_vcf@fix)
 head(pre_vcf@fix)
 
-# Extract GT order
+# Extract marker order in pre-imputed VCF
 vcf_genos.df <- paste0(pre_vcf@fix[, "CHROM"], "__", pre_vcf@fix[, "POS"])
 vcf_genos.df <- as.data.frame(vcf_genos.df)
 colnames(vcf_genos.df) <- "mname"
@@ -60,10 +65,17 @@ replacement_genos.df <- merge(x = vcf_genos.df, y = ai2_output.df, by = "mname",
 dim(replacement_genos.df)
 replacement_genos.df[1:5,1:5]
 
+# Note: The matrix should be in the correct order for the markers/marker names in the VCF
+
 # Make new 'Format' column (that says GT only)
 # Replace the mname column with the FORMAT
 colnames(replacement_genos.df) <- gsub(pattern = "mname", replacement = "FORMAT", x = colnames(replacement_genos.df))
 replacement_genos.df$FORMAT <- rep("GT", times = nrow(replacement_genos.df))
+replacement_genos.df[1:5,1:5]
+
+# Put the columns in the same order as expected from the pre-impute VCF (ai2 shifted them)
+replacement_genos.df <- replacement_genos.df[, colnames(pre_vcf@gt)]
+replacement_genos.df[1:5,1:10]
 
 # Replace the GT section of the VCF
 dim(pre_vcf@gt)
